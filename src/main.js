24adfe27aa481df7448dd14612c43cb98eaca1be
Idimcol1 <idimcol2@gmail.com>
2025-06-12 11:58:28 -0500

    main js

--- src/main.js
+++ src/main.js
@@ -1,5 +1,18 @@
 import { createApp } from 'vue'
+import { Quasar } from 'quasar'
+import '@quasar/extras/material-icons/material-icons.css'
+import 'quasar/src/css/index.sass'
+import {router} from "./routes/routes.js"
 import './style.css'
 import App from './App.vue'
+import langEs from 'quasar/lang/es' // Importa el idioma español
 
-createApp(App).mount('#app')
+
+
+const MyApp = createApp(App)
+MyApp.use(router)
+MyApp.use(Quasar,{
+    plugins: {},
+    lang:langEs
+})
+MyApp.mount('#app')
